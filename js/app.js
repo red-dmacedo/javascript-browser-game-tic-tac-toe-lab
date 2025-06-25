0d2d943aeddd7a0a91ab539178488a0390016996
dmacedo <devinm3333@outlook.com>
2025-06-25 15:54:06 -0700

    Lab Complete v2.0

--- js/app.js
+++ js/app.js
@@ -1,110 +1,149 @@
-// -------------------------------- Constants --------------------------------
-const game = {};
-game.moves = [];
-// 0 1 2
-// 3 4 5
-// 6 7 8
-const winConditions = [
+/*-------------------------------- Constants --------------------------------*/
+const playerSymbols = ['🥸','👾'];
+const winningCombos = [
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
   [0,1,2], [3,4,5], [6,7,8], // Rows
   [0,3,6], [1,4,7], [2,5,8], // Columns
   [0,4,8], [2,4,6] // cross
 ];
-// ---------------------------- Variables (state) ----------------------------
-game.firstTurn = 'X';
-messageContentOld = '';
-game.disabled = false;
-
-// ------------------------ Cached Element References ------------------------
+/*---------------------------- Variables (state) ----------------------------*/
+let board = ['','','','','','','','',''];
+let turn = playerSymbols[0];
+let winner = false;
+let tie = false;
+let disabled = false;
+let comPlayer = true;
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.querySelector('#message');
-const sqrsEl = document.querySelectorAll('.sqr');
 const boardEl = document.querySelector('.board');
-
-
-// -------------------------------- Functions --------------------------------
-game.setMessage = function (msg='Your turn'){ messageEl.textContent = msg; }; // easily set message text
-game.reset = () => {
-  sqrsEl.forEach(i => {
-    i.removeAttribute('style'); // remove opacity setting from all sqrs
-    i.textContent = ""; // clear 'X' and 'O' selections
-  });
-  game.moves.length = 0; // clear moves
-  game.currentTurn = game.firstTurn; // reset current player
-  messageContentOld = `It is ${game.currentTurn}'s turn`;
-  game.disabled = false;
-  // game.setMessage(`It is ${game.currentTurn}'s turn`); // initialize message
+const resetBtnEl = document.querySelector('#reset');
+const comBtn = document.querySelector('#com-button');
+const titleEl = document.querySelector('body h1');
+/*-------------------------------- Functions --------------------------------*/
+function init(){
+  titleEl.textContent = `${playerSymbols[0]} vs. ${playerSymbols[1]}`;
+  tie = false;
+  winner = false;
+  disabled = false;
+  turn = playerSymbols[0];
+  board = ['','','','','','','','',''];
+  for (i of squareEls){ i.removeAttribute('style') };
+  render();
 };
-game.handleBoard = (e, target) => {
-  if(game.disabled){ return; }; // do nothing if the game has completed
-  if(!target){target = e.target} // focus on using target instead of the event (helps with comChoice)
-  if( game.moves.includes(target.id) && game.currentTurn === 'X' ){
-    messageEl.textContent = 'Invalid move'; // Only show invalid moves for the user
-    return;
+
+function render(){
+  updateBoard();
+  updateMessage();
+};
+
+function updateBoard(){
+  for( i=0; i<board.length; i++ ){
+    squareEls[i].textContent = board[i];
+    // set color of corresponding square according to the player who selected it
+    switch( squareEls[i].textContent ){
+      case playerSymbols[0]:
+        squareEls[i].style.backgroundColor = "lightblue";
+        break;
+
+      case playerSymbols[1]:
+        squareEls[i].style.backgroundColor = "pink";
+        break;
+
+      default:
+        break;
+    };
   };
-  game.moves.push(target.id); // track id's of sqrs that have been picked
-  target.textContent = game.currentTurn; // Set sqr to player's symbol 'X'/'O'
-  game.toggleSelection(target, false); // When a sqr is selected, reduce opacity to 50%
-  game.setMessage(`It is ${game.currentTurn}'s turn`); // Update message
-  if (game.currentTurn === 'X') { // swap player and com turns
-    game.currentTurn = 'O';
-    game.comMove(); // com to makes a move
-  } else { game.currentTurn = 'X';};
-  game.winCheck();
-  if(game.moves.length === 9){ game.gameOver() };
 };
-game.toggleSelection = (target, enable=true) => {
-  (enable) ? target.removeAttribute('style') : target.style.opacity = '0.5';
+
+function updateMessage(){
+  if( !(winner) && !(tie) ){ msg = `${turn}'s turn` }
+  else if( !(winner) && tie ){ msg = 'Tie' }
+  else { msg = `${turn} Wins!` };
+  messageEl.textContent = msg;
 };
-game.randomNumGen = () => { return Math.floor(Math.random() * sqrsEl.length); };
-game.comMove = () => {
+
+function handleClick(e, t){ // events and targets may be passed in (assists with comChoice)
+  if( !t ){ t = e.target }; // set target if it was not passed (allows use of elements from quereySelector)
+  
+  if( // Every condition that will exit this function early
+    t.textContent === playerSymbols[0] || // space is taken
+    t.textContent === playerSymbols[1] || // space is taken
+    disabled || // game is over
+    t.className !== 'sqr' // incorrect selection
+  ){ return; };
+  const squareIndex = t.id;
+  placePiece(squareIndex);
+  updateBoard();
+  checkForWinner();
+  checkForTie();
+  switchPlayerTurn();
+  updateMessage();
+  if( comPlayer && turn === playerSymbols[1] ){ setTimeout(comMove, 500); }; // com to makes a move
+};
+
+function placePiece(idx){ board[idx] = turn; };
+
+function checkForWinner(){
+  for(i of playerSymbols){
+    for ( j of winningCombos ){
+      if(
+        board[ j[0] ] === i &&
+        board[ j[1] ] === i &&
+        board[ j[2] ] === i
+      ){
+        winner = true;
+        disabled = true;
+        return;
+      };
+    }
+  };
+};
+
+function checkForTie(){ if( !(board.includes('')) ){ tie = true } };
+
+function switchPlayerTurn(){
+  if( winner ){ return };
+  ( turn === playerSymbols[0] ) ? turn = playerSymbols[1] : turn = playerSymbols[0];
+};
+
+function randomNumGen() { return Math.floor(Math.random() * board.length); };
+
+function comMove() {
   let num = 0;
-  if (game.moves.length < 9 && game.disabled === false){ // prevent the following from running if the game is complete
-    do{ num = game.randomNumGen(); } while ( game.moves.includes(String(num)) ); // game.moves is holding string values
-    comChoice = sqrsEl[num];
-    game.handleBoard('', comChoice);
+  if ( board.includes('') && !(disabled) ){ // prevent the following from running if the game is complete
+    
+    do{ num = randomNumGen(); } while ( board[num] !== '' ); // find an open position on the board
+    let comChoice = squareEls[num];
+    handleClick('', comChoice);
   }
 };
-function messageHover () {
-  messageContentOld = messageEl.textContent; // store previous message so that it can be restored
-  messageEl.textContent = 'Reset game?'; // change text when player moves mouse over the message
-};
-function messageHoverLeave(){ messageEl.textContent = messageContentOld; }; // restore old message
-game.winCheck = () => {
-  let userMoveIds = Array.from(sqrsEl).filter( i => game.moves.includes(i.id) && i.textContent === 'X' ).map(i => i.id);
-  let comMovesIds = Array.from(sqrsEl).filter( i => game.moves.includes(i.id) && i.textContent === 'O' ).map(i => i.id);
-  
-  if( game.currentTurn === 'X' && game.isWinner(userMoveIds) ){
-    game.setMessage('You Win! | Reset Game?');
-    game.disabled = true;
-  };
-  if( game.currentTurn === 'O' && game.isWinner(comMovesIds) ){
-    game.setMessage('Com Wins! | Reset Game?');
-    game.disabled = true;
-  };
-};
-game.isWinner = (moveIds) => {
-  // if(game.currentTurn === 'X'){console.log(moveIds); console.log('typeof:',typeof(moveIds[0]))};
-  let breakLoop = false;
-  winConditions.forEach((i) => {
-    // if(game.currentTurn === 'X'){console.log((i[0].toString()), i[1], i[2])};
-    // if(game.currentTurn === 'X'){console.log(i);}
-    console.log(moveIds.includes(i[0].toString()))
-    if(
-      moveIds.includes(i[0].toString()) &&
-      moveIds.includes(i[1].toString()) &&
-      moveIds.includes(i[2].toString())
-    ) { breakLoop = true; };
-  });
-  return breakLoop;
-};
-game.gameOver = () => {
-  game.setMessage('Game Over! (Tie) | Reset Game?');
-  game.disabled = true;
-};
-// ----------------------------- Event Listeners -----------------------------
-boardEl.addEventListener('click', game.handleBoard);
 
-// --------------------------------- Script ----------------------------------
-game.reset();
-messageEl.addEventListener('click', game.reset);
-messageEl.addEventListener('mouseenter', messageHover);
-messageEl.addEventListener('mouseout', messageHoverLeave);
+function handleComBtn(e){
+  const comText = ['Com Player On', 'Com Player Off'];
+  if(e.target.textContent === comText[0]){
+    e.target.textContent = comText[1];
+    comPlayer = false;
+  } else {
+    e.target.textContent = comText[0];
+    if( !disabled && turn === playerSymbols[1] ) { comMove(); };
+    comPlayer = true;
+  };
+};
+/*----------------------------- Event Listeners -----------------------------*/
+resetBtnEl.addEventListener('click', init);
+boardEl.addEventListener('click', handleClick);
+comBtn.addEventListener('click', handleComBtn)
+/*---------------------------------- Script ---------------------------------*/
+init();
+// ===== Minimum Requirements ===== \\
+// Display an empty tic-tac-toe board when the page is initially displayed.
+// A player can click on the nine cells to make a move.
+// Every click will alternate between marking an X and O.
+// Display whose turn it is (X or O).
+// The cell cannot be played again once occupied with an X or O.
+// Provide win logic and display a winning message.
+// Provide logic for a cat’s game (tie), also displaying a message.
+// Provide a Reset Game button that will clear the contents of the board.
